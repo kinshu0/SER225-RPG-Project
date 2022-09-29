2f846466fd3aaacbe76727ea39fdebaa2b852c85
Eric Carlson <eric.mark.carlson@gmail.com>
2022-09-29 14:50:44 -0400

    additions of weapons

--- src/Maps/TestMap.java
+++ src/Maps/TestMap.java
@@ -1,5 +1,9 @@
 package Maps;
 
+import EnhancedMapTiles.Axe;
+import EnhancedMapTiles.Katana;
+import EnhancedMapTiles.Machete;
+import EnhancedMapTiles.Spear;
 import EnhancedMapTiles.Rock;
 import Level.EnhancedMapTile;
 import Level.Map;
@@ -37,6 +41,10 @@ public class TestMap extends Map {
     public ArrayList<EnhancedMapTile> loadEnhancedMapTiles() {
         ArrayList<EnhancedMapTile> enhancedMapTiles = new ArrayList<>();
         enhancedMapTiles.add(new Rock(getMapTile(2, 7).getLocation()));
+        enhancedMapTiles.add(new Axe(getMapTile(4, 6).getLocation()));
+        enhancedMapTiles.add(new Katana(getMapTile(3, 5).getLocation()));
+        enhancedMapTiles.add(new Machete(getMapTile(3, 7).getLocation()));
+        enhancedMapTiles.add(new Spear(getMapTile(3, 9).getLocation()));
         return enhancedMapTiles;
     }
 
